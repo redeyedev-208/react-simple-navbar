e945b82d3da9a163de126df531a5e10887f49a01
gilaguilar <redeyedev.208@gmail.com>
2023-04-23 01:19:28 -0600

    feat: simple react responsive navbar with hamburger

---
+++ src/components/About.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const About = () => {
+  return <div className="container">About Page</div>;
+};
+
+export default About;
---
+++ src/components/Skills.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Skills = () => {
+  return <div className="container">Skills Page</div>;
+};
+
+export default Skills;
